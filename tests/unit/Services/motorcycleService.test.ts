4f1c431b89a186761d9d5138b142e88ae1403649
EduardoBalbinot <914thepro@gmail.com>
2023-05-10 18:57:31 -0300

    requisito 9 feito

--- tests/unit/Services/motorcycleService.test.ts
+++ tests/unit/Services/motorcycleService.test.ts
@@ -27,11 +27,39 @@ const output: Motorcycle = new Motorcycle({
 });
 
 describe('Testa a camada service de motorcycle', function () {
+  afterEach(function () {
+    sinon.restore();
+  }); 
   it('inserindo uma motocicleta com sucesso', async function () {
     sinon.stub(Model, 'create').resolves(output);
 
     const service = new MotorcycleService();
     const response = await service.insertMotorcycle(input);
     expect(response).to.be.deep.equal(output);
+  });
+
+  it('buscando uma motocicleta especifica', async function () {
+    sinon.stub(Model, 'findById').resolves(output);
+
+    const service = new MotorcycleService();
+    const result = await service.getMotorcycleById('645c081534e4f3e083c27b93');
+    expect(result).to.be.deep.equal(output);
+  });
+
+  it('busca todas as motocicletas', async function () {
+    const manyMotorcyclesOutput = [{ ...output }, { ...output }];
+    sinon.stub(Model, 'find').resolves(manyMotorcyclesOutput);
+
+    const service = new MotorcycleService();
+    const result = await service.getMotorcycles();
+    expect(result).to.be.deep.equal(manyMotorcyclesOutput);
+  });
+
+  it('buscando uma motocicleta que não existe', async function () {
+    sinon.stub(Model, 'findById').resolves(null);
+
+    const service = new MotorcycleService();
+    const response = await service.getMotorcycleById('644a76b413b85a4facd8d42e');
+    expect(response).to.equal(null);
   });
 });
